8fcc1fdeb1d1d8801d7760540f71dbfcc9c473dc
lxq <lxq271332@alibaba-inc.com>
2025-11-04 14:14:15 +0800

    feat: add connector interface

---
+++ rtp_llm/cpp/cache_new/KVCacheConnector.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "rtp_llm/cpp/cache/BatchKVCacheResource.h"
+
+namespace rtp_llm {
+
+class KVCacheConnector {
+public:
+    KVCacheConnector()          = default;
+    virtual ~KVCacheConnector() = default;
+
+public:
+    using CallBack = std::function<void(bool)>;
+
+public:
+    virtual bool init() = 0;
+    virtual void asyncGet(const BatchKVCacheResourcePtr& resource, const CallBack& callback) = 0;
+    virtual void asyncPut(const BatchKVCacheResourcePtr& resource, const CallBack& callback) = 0;
+};
+
+}  // namespace rtp_llm
